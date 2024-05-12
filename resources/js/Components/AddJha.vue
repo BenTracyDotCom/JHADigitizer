d31d03d65ca200471ab7d24bcc3fc7bee88ccaa9
Ben Tracy <b.rob.tracy@gmail.com>
2024-05-12 08:48:36 -0400

    render step adder once jha is created

--- resources/js/Components/AddJha.vue
+++ resources/js/Components/AddJha.vue
@@ -7,16 +7,31 @@ const jha = ref(null);
 const steps = ref([]);
 
 async function addJha() {
-  const { data } = await axios.post(`${import.meta.env.VITE_APP_URL}/api/jhas`);
+  const { data } = await axios.post(`${import.meta.env.VITE_APP_URL}/api/jhas`, {
+    title: "Test JHA",
+    author: "Brengeley"
+  });
   jha.value = data;
 }
 
 export default {
-  name: 'AddJha',
+  name: "AddJha",
   methods: {
     close() {
-      this.$emit('close');
+      this.$emit("close");
     },
+    sendJha() {
+      addJha()
+    }
+  },
+  data() {
+    return {
+      schema: {
+        title: { type: "text", label: "Title" },
+        author: { type: "text", label: "Author" },
+      },
+      jha: jha
+    };
   },
 };
 </script>
@@ -25,20 +40,16 @@ export default {
   <div class="fixed inset-0 bg-black/50 flex justify-center items-center">
     <div class="shadow-lg overflow-y-auto flex flex-col bg-white rounded-lg w-4/6 h-5/6">
       <!-- <input v-model="title" placeholder="Add Title" /> -->
-      <Vueform>
-        <TextElement name="hello_world" label="Hello" placeholder="World" />
-      </Vueform>
-        
-        <div class="border-2">
-          <button
-              type="button"
-              class="bg-green-300 border-2 rounded-md"
-              @click="close"
-            >
-              Close Modal
-            </button>
-        </div>
-  
+      <Vueform :schema="schema"></Vueform>
+      <button type="button" @click="sendJha">
+        Create JHA form
+      </button>
+      <div v-if="jha">You can now add steps to the jha.</div>
+      <div class="border-2">
+        <button type="button" class="bg-green-300 border-2 rounded-md" @click="close">
+          Close Modal
+        </button>
+      </div>
     </div>
   </div>
 </template>
