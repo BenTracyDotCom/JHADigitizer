3f0c9d730ce920f170d665b729c1afc19ee294a8
Ben Tracy <b.rob.tracy@gmail.com>
2024-05-12 14:53:31 -0400

    add hazards to steps

---
+++ resources/js/Components/AddableHazard.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from "axios";
+import { ref } from "vue";
+
+const hazard = ref(null);
+
+async function postHazard({ step_id, title }) {
+  const toSend = {
+    step_id: step_id,
+    title: title,
+  };
+
+  const { data } = await axios.post(`${import.meta.env.VITE_APP_URL}/api/hazards`, toSend);
+  hazard.value = data;
+
+}
+export default {
+  name: 'AddableHazard'
+}
+</script>
+
+<template>
+  <div>
+    Hazard goes here
+  </div>
+</template>
--- resources/js/Components/AddableStep.vue
+++ resources/js/Components/AddableStep.vue
@@ -1,17 +1,18 @@
 <script>
 import axios from "axios";
 import { ref } from "vue";
+import AddableHazard from "../Components/AddableHazard.vue"
 
 const step = ref(null);
 
-async function addStep({ jha_id, title }) {
+async function postStep({ jha_id, title }) {
   const toSend = {
     jha_id: jha_id,
     title: title,
   };
 
   const { data } = await axios.post(`${import.meta.env.VITE_APP_URL}/api/steps`, toSend);
-  return data;
+  step.value = data;
 }
 
 export default {
@@ -19,6 +20,9 @@ export default {
   props: {
     id: Number,
     index: Number,
+  },
+  components: {
+    AddableHazard
   },
   data() {
     return {
@@ -28,7 +32,14 @@ export default {
   },
   methods: {
     addStep() {
-
+      const form = this.$refs.form$.data
+      form.jha_id = this.id
+      postStep(form)
+    },
+    handleAddHazard() {
+        console.log('clicked')
+        console.log(this.hazards)
+        this.hazards[this.hazards.length] = this.hazards.length + 1;
     },
     // addStep() {
     //   if(this.$refs.form$.every($form => ($form.data.title))){
@@ -42,7 +53,8 @@ export default {
 <template>
   <div class="">
     <div>Step {{ index + 1 }}</div>
-      <Vueform ref="form$">
+    <div v-if="step">{{ step.title }}</div>
+      <Vueform ref="form$" @submit="addStep" v-if="!step">
         <GroupElement
         name="step"
         >
@@ -51,7 +63,13 @@ export default {
           placeholder="Description"
           rules="required"/>
         </GroupElement>
+        <ButtonElement v-if="!step" name="submit" submits>
+          Save
+        </ButtonElement>
       </Vueform>
-    <div class="bg-yellow-500" @click="addStep">Add hazards</div>
+    <div class="bg-yellow-500" @click="handleAddHazard" v-if="step">Add hazards</div>
+    <div v-if="hazards" v-for="(step, index) in hazards" :key="index">
+      <AddableHazard v-bind="step" :index="index" />
+    </div>
   </div>
 </template>
